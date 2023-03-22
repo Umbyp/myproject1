23aea8b702f8f4ef5d109b449dc48c22703dd2d0
Umbyp <Student@5CD3019B3H.control.ad.sit.kmutt.ac.th>
2023-03-22 10:47:51 +0700

    modified file

--- MyProject1.java
+++ MyProject1.java
@@ -12,6 +12,6 @@ package com.mycompany.myproject1;
 public class MyProject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
